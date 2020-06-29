7a9847938cf8b12f7a1b824daf61c3acfd8f16b4
Sheeba0911 <sheeba.silviya@gmail.com>
2020-06-29 21:58:45 +0530

    added another check for ok

--- learninggit.py
+++ learninggit.py
@@ -4,9 +4,11 @@ Created on Mon Jun 29 20:53:40 2020
 
 @author: Sheebu
 """
-I=input("How was your learning today (good/bad)")
+I=input("How was your learning today (good/bad/ok)")
 if I=="good":
     print("you are always a rock star")
+elif I=="ok":
+    print("Have a hot cup of coffee")
 else:
     print("Your quest is still on")
     
