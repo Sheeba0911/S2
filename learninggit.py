5a15b0777d76ac7f6dbf1dd50ec871e88fb3d66b
Learngit <sheeba.silviya@gmail.com>
2020-06-29 21:42:54 +0530

    my first version of test code

---
+++ learninggit.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 20:53:40 2020
+
+@author: Sheebu
+"""
+I=input("How was your learning today (good/bad)")
+if I=="good":
+    print("you are always a rock star")
+else:
+    print("Your quest is still on")
+    
+
